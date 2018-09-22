ce1a1499308b40a408c2fad1a86141cc7510553a
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-09-22 22:01:49 +0530

    Update and rename README.md to Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+  public class Ascii{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter a Charecter:");
+      char in=sc.next().charAt(0);
+      int Conv=(int)in;
+      System.out.println("ANS:"+Conv);
+      }
+    }  
